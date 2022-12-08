6bf5a5dc284cf9bf823e8b0437a87793dccabff6
Hugo Martin <hugomartin.menz@gmail.com>
2022-12-08 15:39:49 +0100

    Corrected ratio and price feed in client.py

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # JPMC Task 1
-Starter repo for task 1 of the JPMC software engineering program
+
+### Creating the virtual environment
+
+In the terminal:
+
+`> python -m venv venv`
+
+Now we will see semething like (venv) C:\your\project\path\jpmc-task=1
+
+### Installing requirements.txt
+
+`pip install -r requirements.txt`
+
+<hr>
+
+## Task
+
+### Purpose
+
+We want to process the data feed of stock A and stock B’s price to enable us to analyse when trading for the stock should occur.
+
+### Acceptance Criteria
+
+getDataPoint function should return correct tuple of stock name, bid_price, ask_price and price. Note: price of a stock = average of bid and ask
+getRatio function should return the ratio of the two stock prices
+main function should output correct stock info, prices and ratio
--- client.py
+++ client.py
@@ -22,38 +22,38 @@ import json
 import random
 import urllib.request
 
-# Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+QUERY = "http://localhost:8080/query?id={}" # Server API URLs
 
-# 500 server request
-N = 500
-
+N = 500 # 500 server request
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    
+    if price_b == 0: return # when price_b is 0 avoid throwing ZeroDivisionError
 
+    return price_a/price_b
 
 # Main
 if __name__ == "__main__":
+    round_number = lambda num, dec : round(num, dec)
+
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
+        
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
+            print(f'Quoted {stock} at (bid: {bid_price}, ask: {ask_price}, price: {round_number(price, 2)})')
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % round_number(getRatio(prices['ABC'], prices['DEF']), 5))
